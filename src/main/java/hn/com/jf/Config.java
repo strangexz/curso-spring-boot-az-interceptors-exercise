738db20798d77232347d9ba3bd88200d562f5859
Strangexz <ja_figueroa@outlook.es>
2024-08-23 11:44:51 -0600

    refactor: mejorada la validacion del interceptor

--- src/main/java/hn/com/jf/Config.java
+++ src/main/java/hn/com/jf/Config.java
@@ -1,11 +1,24 @@
 package hn.com.jf;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @PropertySources({ @PropertySource("classpath:config.properties") })
-public class Config {
+public class Config implements WebMvcConfigurer {
+	@Autowired
+	@Qualifier("calendarInterceptor")
+	private HandlerInterceptor calendar;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(calendar).addPathPatterns("/**");
+	}
 
 }
--- src/main/java/hn/com/jf/interceptors/CalendarInterceptor.java
+++ src/main/java/hn/com/jf/interceptors/CalendarInterceptor.java
@@ -1,6 +1,7 @@
 package hn.com.jf.interceptors;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,7 +15,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@Component
+@Component("calendarInterceptor")
 public class CalendarInterceptor implements HandlerInterceptor {
 	@Value("${config.calendar.open}")
 	private Integer open;
@@ -27,37 +28,39 @@ public class CalendarInterceptor implements HandlerInterceptor {
 			throws Exception {
 		Calendar calendar = Calendar.getInstance();
 		int hour = calendar.get(Calendar.HOUR_OF_DAY);
-		System.out.println(hour);
-		
+		System.out.println("open = " + open);
+		System.out.println("close = " + close);
+		System.out.println("hour = " + hour);
+
 		if (hour >= open && hour < close) {
 			StringBuilder message = new StringBuilder("Bienvenidos al horario de atención al Cliente");
 			message.append(", atendemos desde las ");
 			message.append(open);
-			message.append("hrs. hasta las");
+			message.append("hrs. hasta las ");
 			message.append(close);
-			message.append("hrs.");
-			message.append("Gracias por su visita!");
-			
+			message.append("hrs. Gracias por su visita!");
+
 			request.setAttribute("message", message.toString());
 			return true;
 		}
-		
+
 		ObjectMapper mapper = new ObjectMapper();
 		Map<String, Object> json = new HashMap<>();
-		StringBuilder message = new StringBuilder("Fuera del horario de atención!");
-		
+		StringBuilder message = new StringBuilder("Fuera del horario de atención! ");
+
 		message.append("Visitenos desde las ");
 		message.append(open);
 		message.append("hrs. Hasta las ");
 		message.append(close);
 		message.append("hrs. Gracias por su visita!");
-		
+
+		json.put("date", new Date().toString());
 		json.put("message", message.toString());
-		
+
 		response.setContentType("application/json");
 		response.setStatus(401);
-		response.getWriter().write(mapper.writeValueAsString(message));
-		
+		response.getWriter().write(mapper.writeValueAsString(json));
+
 		return false;
 	}
 
